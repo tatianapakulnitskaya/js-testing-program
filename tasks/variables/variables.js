f4069e9c1dbc1fd8363a4afd982529ca4e3be763
Tatiana Pakulnitskaya <Tatiana_Pakulnitskaya@epam.com>
2020-06-15 13:59:52 +0300

    lection2_full

--- tasks/variables/variables.js
+++ tasks/variables/variables.js
@@ -1,38 +1,38 @@
 /**
  * TODO create variable named int and set value equal to 1
 */
-let int = 1;
+var int = 1;
 /**
  *  TODO create variable named int2 and set value equal int plus 1
  */
-let int2 = int+1;
+var int2 = int+1;
 /**
  * TODO create variable named str and set value equal to "John"
  */
-let str = 'John';
+var str = 'John';
 /**
  * TODO create variable named str2 and set value equal str plus " Dou"
  */
-let str2 =str + ' Dou';
+var str2 =str + ' Dou';
 /**
  * TODO create variable named bool and set value equal to comparison of int and int2 variables
  */
-let bool = int==int2;
+var bool = int==int2;
 /**
  * TODO create variable named arr and set value array of numbers from 1 to 5
  */
-let arr = [];
-for (let i = 1; i < 6; i++) {
+var arr = [];
+for (var i = 1; i < 6; i++) {
   arr[i] = i;
 }
 /**
  * TODO create variable named fifth and set value as element of array with index 4
  */
-let fifth = arr[4];
+var fifth = arr[4];
 /**
  * TODO create variable named human and set value as object with key firstName and value "John"
  */
-let human = {firstName: "John"};
+var human = {firstName: "John"};
 /**
  * TODO set new key of human object named lastName and value "Dou"
  */
