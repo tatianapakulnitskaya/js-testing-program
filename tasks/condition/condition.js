9440027ba1f2a5d344e982a536bee008426ee7f5
Tatiana Pakulnitskaya <Tatiana_Pakulnitskaya@epam.com>
2020-06-23 12:42:34 +0300

    Lection3_final

--- tasks/condition/condition.js
+++ tasks/condition/condition.js
@@ -4,7 +4,7 @@
  * Task - rewrite IF..ELSE to ternary operator
  */
 {
-  const ab = 3,
+  const a = 3,
         b = 2;
 
   if (a + b < 4) {
@@ -13,6 +13,10 @@
     console.log('too much');
   }
 }
+
+const a = 3,
+      b = 2;
+const condValue1 = (a + b < 4) ? console.log('wrong') : console.log('too much');
 
 /** TODO
  * The system receives two params x and y.
@@ -23,7 +27,22 @@
 {
     const x = 3;
     const y = 7;
+
+    if (x<10 && y<10) {
+      const z = x+y;
+      //console.log(z);
+    } else {
+      const z = x*y;
+      //console.log(z);
+    }
+   
 }
+
+const x = 3;
+const y = 7;
+let z;
+const condValue2 = (x<10 && y<10) ? z = x+y : z = x*y;
+//console.log(z);
 
 /** TODO
  * The system receives 3 params - x, y, operator.
@@ -35,4 +54,25 @@
     const x = 3;
     const y = 7;
     const operator = "add";
+    let z;
+
+    switch(operator){
+      case "add":
+        z = x+y;
+        //console.log(z);
+        break;
+      case "subtract":
+        z = x-y;
+        //console.log(z);
+        break;
+      case "multiply":
+        z = x*y;
+        //console.log(z);
+        break;
+      case "divide":
+        z = x/y;
+        //console.log(z);
+        break;
+    }
 }
+
