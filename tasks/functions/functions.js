fe59c46ec4893d428e2b0b16e2387cc9deb76de5
Tatiana Pakulnitskaya <Tatiana_Pakulnitskaya@epam.com>
2020-06-30 10:25:42 +0300

    lection4_full

--- tasks/functions/functions.js
+++ tasks/functions/functions.js
@@ -3,9 +3,10 @@
  *
  */
 function add(a, b) {
-
+	var result = a + b;
+	return result;
 }
-
+console.log(add(2, 3));
 /**
  * write function that return first and last name of given object
  * properties firstName and lastName
@@ -14,33 +15,62 @@ function add(a, b) {
  *    lastName: "Dou"
  * }
  */
+var person = {
+
+	firstName: "John",
+	lastName: "Dou"
+};
+
 function getFullName(object) {
 
+	return object.firstName + " " + object.lastName;
+
 }
+console.log(getFullName(person));
 
 /**
  * write fuction that checks is number is odd
  * true if odd, false if even
  */
 function isOdd(n) {
-
+	if (n % 2 == 0)
+		return false;
+	else
+		return true;
 }
-
+console.log(isOdd(3));
 /**
  * write function that return shortest of words in given array
  * e.g ["one", "two", "three"] should return one
  */
 function getShortest(wordArray) {
+	let shortest = null;
+	for (i = 0; i < wordArray.length; i++) {
+		if (shortest == null)
+			shortest = wordArray[i];
+		else if (wordArray[i].length < shortest.length)
+			shortest = wordArray[i];
 
+	}
+	return shortest;
 }
-
+console.log(getShortest(["one", "two", "three"]));
 /**
  * write function that returns word google with given numbers of "o" symbols
  * e.g getGoogle(5) should return "gooooogle"
  */
 function getGoogle(n) {
+	let origString = "ggle";
+	let stringToAdd = "o".repeat(n);
+	let indexPosition = 1;
+	newString = origString.slice(0, indexPosition) +
+		stringToAdd +
+		origString.slice(indexPosition);
 
+	return newString;
 }
+
+console.log(getGoogle(5));
 
 /**
  * write function that returns object based on given information (params may be null)
@@ -51,20 +81,43 @@ function getGoogle(n) {
  *    age: 42
  * }
  */
-function getUser(firstName, lastName, age) {
+function getUser(firstName = null, lastName = null, age = null) {
+
+	var object = {
+		firstname: firstName,
+		lastname: lastName,
+		age: age
+	};
+
+	return object;
+
 
 }
-
+console.log(getUser("John", "Dou", 42));
 /**
  * write function that calculates total path traveled.
  * path represended as array of objects with field distance and direction
  * e.g [{direction: "Kiyv - Minsk", distance: 567}, {direction: "Kiyv - Paris", distance: 2402}]
  */
 
+var path = [{
+		direction: "Kiyv - Minsk",
+		distance: 567
+	},
+	{
+		direction: "Kiyv - Paris",
+		distance: 2402
+	}
+]
+
 function getTotalPath(path) {
-
+	let total = 0;
+	for (i = 0; i < path.length; i++) {
+		total = total + path[i].distance;
+	}
+	return total;
 }
-
+console.log(getTotalPath(path));
 /**
  * write a function that will calculate a discount considering the Amount
  * and the percentage (hint: you need to use the Closure here)
@@ -73,9 +126,13 @@ function getTotalPath(path) {
  */
 
 function discountFunction(amount) {
-
+	return function (percentage) {
+		return percentage - amount * percentage * 0.01;
+	}
 }
 
+const discountFunction10 = discountFunction(10);
+console.log(discountFunction10(90));
 /**
  * Write the methods inside the given objects that:
  * 1. console logs keys of the given object
@@ -90,12 +147,16 @@ const myObject = {
 	friends: ['Mike', 'Alan', 'Daniel'],
 	keys() {
 		//write your code here
+		console.log(Object.keys(myObject));
 	},
 	call() {
 		//write your code here
+		console.log("My name is " + this.name + " " + this.lastName + " and I am " + this.age + " years old. My best friend is " + this.friends[2]);
 	}
-
 };
+myObject.keys();
+myObject.call();
+
 
 module.exports = {
 	add,
